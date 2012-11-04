cd3e751b2f0ec010aecffbc7bbdc057ccdec06ab
Aravind Pedapudi <aravindp1510@gmail.com>
2012-11-04 12:29:10 +0530

    Added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+#Temporarily Bypass Fortiguard
+#####Chrome extension to do that automatically
+
+This extension is called whenever a fortiguard page is found, which then redirects through a proxy server. 
+
+######Stuff that needs to be blocked has to stay blocked.
+This extension was created for the benefit of many. The proxies are bound to get blocked someday but you can delay it if you use it with discretion.
--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   var webBlocked=$("body font table tbody tr td font b");
+  var wlocation = window.location.href;
   if(webBlocked.text()=="Web Page Blocked"){
-    var message="Fortiguard Sucks"; 
+    var message="Fortiguard Sucks. Redirecting you through proxy: "; 
     $("body").html(message);
+    window.location = "http://www.cheatfusd.appspot.com/"+wlocation.substring(7);
   }
 });
